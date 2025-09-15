36ecfbe0ae5e580d0982b6eae5c0e682b8e4f1fe
Intelligent Resume and LinkedIn Feedback System <A235474789>
2025-09-15 14:37:04 +0530

    debug

--- login-ui/backend/db_helpers.py
+++ login-ui/backend/db_helpers.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["lpu_bot"]
 users_col = db["users"]
