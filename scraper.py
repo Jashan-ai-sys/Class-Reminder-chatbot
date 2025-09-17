e84dd4fc02e0bf41a192d80c56687ab0b414e8b4
Intelligent Resume and LinkedIn Feedback System <A235474789>
2025-09-17 14:42:21 +0530

    debug

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import time
 import requests
 from datetime import datetime
 from dotenv import load_dotenv
-from playwright.async_api import sync_playwright
+from playwright.async_api import async_playwright
 
 from db_helpers import get_user, save_cookie
 from crypto import decrypt_password
