3b6a7a4bdfa3544168ed24aca5075f55f0f6e04f
Intelligent Resume and LinkedIn Feedback System <A235474789>
2025-09-18 19:03:17 +0530

    structured the files

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-# ============== CORE PYTHON LIBRARIES ==============
 import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import asyncio
 import logging
