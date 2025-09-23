6a3879e09c7e1cb946709a1f3821091e34577eb6
Intelligent Resume and LinkedIn Feedback System <A235474789>
2025-09-23 11:24:22 +0530

    adding buttons

--- common/scraper.py
+++ common/scraper.py
@@ -5,7 +5,7 @@ import aiohttp
 import ssl
 import certifi
 from playwright.async_api import async_playwright
-from playwright_manager import run_in_tab 
+from .playwright_manager import run_in_tab 
 
 # Import the database helpers
 from .db_helpers import get_user, save_cookie
